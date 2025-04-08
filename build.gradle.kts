131b9cf6ad6607e0a909c0588ba6b12dad53e1c0
saurav1811 <saurabhmohapatra23@gmail.com>
2025-04-08 10:01:03 +0530

    Saurav | TG-Coding-Problem | Add initial kotlin-gradle code setup

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/com/technogise/chessgame/Main.kt
@@ -0,0 +1,5 @@
+package com.technogise.chessgame
+
+fun main() {
+    println("Hello Kotlin World!")
+}
